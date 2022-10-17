ae00e4ff87402b8e64e1407f41bc5700e464f509
Tiago Chacon <tiagochacon80@gmail.com>
2022-10-17 15:55:13 -0400

    inserting a field in a table

---
+++ SQL4.1.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_PRODUTOS
+(PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA)
+VALUES 
+('1040107', 'Light - 350 ml - Melância', 'Lata', '350 ml', 'Melância', 4.56);
+
+select * from TB_PRODUTOS;
