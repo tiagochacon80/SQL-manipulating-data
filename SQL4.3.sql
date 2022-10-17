1ab9e06e8a1703a37f665348534f3edb92cc8f1a
Tiago Chacon <tiagochacon80@gmail.com>
2022-10-17 16:17:21 -0400

    inserting several fields

---
+++ SQL4.3.sql
@@ -0,0 +1,16 @@
+INSERT INTO TB_PRODUTOS
+(PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA)
+VALUES
+('1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01);
+
+INSERT INTO TB_PRODUTOS
+(PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA)
+VALUES 
+('1000889', 'Sabor da montanha - 700 ml - Uva', 'Garrafa', '700 ml', 'Uva', 6.31);
+
+INSERT INTO TB_PRODUTOS
+(PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA)
+VALUES
+('1004327', 'Videira do campo - 1,5 Litros - Melância', 'PET', '1,5 Litros', 'Melância',19.51);
+
+SELECT * FROM tb_produtos;
